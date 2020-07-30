904fadd6c3a0ec897203292f8f65bf1bbd2a767e
Luca <lucamug@gmail.com>
2020-07-30 14:11:05 +0900

    Adding lazy

--- src/Pages/PageB.elm
+++ src/Pages/PageB.elm
@@ -3,6 +3,7 @@ module Pages.PageB exposing (Model, Msg, Params, page)
 import Components.Counter
 import Element exposing (..)
 import Element.Font as Font
+import Element.Lazy
 import Shared
 import Spa.Document exposing (Document)
 import Spa.Page as Page exposing (Page)
@@ -76,23 +77,28 @@ subscriptions model =
 -- VIEW
 
 
-view : Model -> Document Msg
-view model =
+subView : Model -> Element Msg
+subView model =
     let
         _ =
             Debug.log "view PageB" ()
     in
+    el [ paddingXY 0 20 ] <|
+        Components.Counter.view
+            { bgColor = rgb255 65 169 208
+            , count = model.count
+            , fgColor = rgb255 0 0 0
+            , msgDecrement = Decrement
+            , msgIncrement = Increment
+            }
+
+
+view : Model -> Document Msg
+view model =
     { title = "PageB"
     , body =
         [ el [ Font.size 32 ] <| text "Page B"
         , text "Another counter:"
-        , el [ paddingXY 0 20 ] <|
-            Components.Counter.view
-                { bgColor = rgb255 65 169 208
-                , count = model.count
-                , fgColor = rgb255 0 0 0
-                , msgDecrement = Decrement
-                , msgIncrement = Increment
-                }
+        , Element.Lazy.lazy subView model
         ]
     }
