d5ac3271516f598be8b3f7a00d710f53f485a9ef
naach <napat_pat3@hotmail.com>
2021-01-15 20:47:29 +0700

    perf: change var to const

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 // Initial data passed to Elm (should match `Flags` defined in `Shared.elm`)
 // https://guide.elm-lang.org/interop/flags.html
-var flags = null
+const flags = null
 
 // Start our Elm application
-var app = Elm.Main.init({ flags: flags })
+const app = Elm.Main.init({ flags: flags })
 
 // Ports go here
 // https://guide.elm-lang.org/interop/ports.html
